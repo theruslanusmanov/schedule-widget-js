76e410e88009577ab33ff31ecc2f4249440ea83e
Ruslan Usmanov <ruslanusm6@gmail.com>
2022-01-16 22:30:23 +0500

    add time info

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 import { END_HOURS, START_HOURS, trackRulerCursor } from './modules/calendar.js'
 
 // Add hours blocks in calendar ruler
+const hoursFragment = document.createDocumentFragment();
 const hours = document.getElementsByClassName(
   'schedule-calendar__ruler__hours')[0]
 for (let i = 0; i < END_HOURS - START_HOURS; i++) {
   const li = document.createElement('li')
-  hours.append(li)
+  hoursFragment.append(li)
 }
+hours.append(hoursFragment);
 
 // Add current time cursor
 const ruler = document.getElementById('rulerId')
--- modules/calendar.js
+++ modules/calendar.js
@@ -1,38 +1,53 @@
 export const name = 'calendar'
 
-const START_HOURS = 7;
-const END_HOURS = 21;
+const START_HOURS = 0
+const END_HOURS = 24
 
 function trackRulerCursor (rulerId) {
-  const cursor = document.getElementById('cursor');
+  const cursor = document.getElementById('cursor')
 
-  const ruler = document.getElementById(rulerId);
-  const rect = ruler.getBoundingClientRect();
+  const ruler = document.getElementById(rulerId)
+  const rect = ruler.getBoundingClientRect()
 
   ruler.addEventListener('mouseenter', () => {
-    cursor.style.display = 'block';
+    cursor.style.display = 'block'
   })
 
   ruler.addEventListener('mouseleave', () => {
-    cursor.style.display = 'none';
+    cursor.style.display = 'none'
   })
 
   ruler.addEventListener('mousemove', event => {
-    const position = event.clientX - rect.left;
+    const position = event.clientX - rect.left
 
     if (position > 0) {
       cursor.style.transform = `translateX(${position}px)`
+
+      // Calculate time
+      const percentage = position / rect.width
+      const oneHour = rect.width / (END_HOURS - START_HOURS)
+      const hours = (Math.floor(position / oneHour)).toLocaleString('en-US', {
+        minimumIntegerDigits: 2
+      })
+      const minutes = (Math.floor(((position / oneHour) % 1).toString().split('.')[1]?.substr(0, 2) * 0.6)).toLocaleString('en-US', {
+        minimumIntegerDigits: 2
+      })
+      const time = `${hours}:${minutes}`
+
+      const info = document.getElementsByClassName(
+        'schedule-container__info')[0]
+      info.innerHTML = time
     }
-  });
+  })
 
   ruler.addEventListener('click', event => {
-    const position = event.clientX - rect.left;
-    console.log(`${Math.floor((position / rect.width) * 100)}%`);
+    const position = event.clientX - rect.left
+    console.log(`${Math.floor((position / rect.width) * 100)}%`)
   })
 }
 
 export {
   trackRulerCursor,
   START_HOURS,
-  END_HOURS
+  END_HOURS,
 }
