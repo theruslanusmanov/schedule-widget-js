f179bdcfac5563d9c2b88af13d89a4ec70de7194
Ruslan Usmanov <ruslanusm6@gmail.com>
2022-01-18 04:40:40 +0500

    add current date on top

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-import  './src/calendar.js'
+import  './src/calendar.js';
+
+const date = new Date();
+const dateElement = document.getElementById('date');
+dateElement.innerText = date.toDateString();
