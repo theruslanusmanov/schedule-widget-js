4e714476e5d4c0f52cf91c545310d1451454a146
Ruslan Usmanov <ruslanusm6@gmail.com>
2022-01-17 19:20:45 +0500

    refactor: remove track function

--- src/calendar.js
+++ src/calendar.js
@@ -150,51 +150,42 @@ customElements.define('schedule-calendar', class Calendar extends HTMLElement {
     const cursor = this.shadowRoot.getElementById('cursor');
     const info = document.getElementsByClassName('schedule-container__info')[0];
 
-    const trackRulerCursor = (rulerId) => {
-      const ruler = this.shadowRoot.getElementById(rulerId);
-      const rect = ruler.getBoundingClientRect();
+    ruler.addEventListener('mouseenter', () => {
+      cursor.style.display = 'block';
+      info.style.visibility = 'visible';
+    });
 
-      ruler.addEventListener('mouseenter', () => {
-        cursor.style.display = 'block';
-        info.style.visibility = 'visible';
-      });
+    ruler.addEventListener('mouseleave', () => {
+      cursor.style.display = 'none';
+      info.style.visibility = 'hidden';
+    });
 
-      ruler.addEventListener('mouseleave', () => {
-        cursor.style.display = 'none';
-        info.style.visibility = 'hidden';
-      });
+    ruler.addEventListener('mousemove', event => {
+      const position = event.clientX - rect.left;
 
-      ruler.addEventListener('mousemove', event => {
-        const position = event.clientX - rect.left;
+      if (position > 0) {
+        cursor.style.transform = `translateX(${position}px)`;
 
-        if (position > 0) {
-          cursor.style.transform = `translateX(${position}px)`;
-
-          // Calculate time
-          const percentage = position / rect.width;
-          const oneHour = rect.width / (END_HOURS - START_HOURS);
-          const hours = (Math.floor(position / oneHour)).toLocaleString('en-US',
-            {
-              minimumIntegerDigits: 2,
-            });
-          const minutes = (Math.floor(
-            ((position / oneHour) % 1).toString().split('.')[1]?.substr(0, 2) *
-            0.6)).toLocaleString('en-US', {
+        // Calculate time
+        const percentage = position / rect.width;
+        const oneHour = rect.width / (END_HOURS - START_HOURS);
+        const hours = (Math.floor(position / oneHour)).toLocaleString('en-US',
+          {
             minimumIntegerDigits: 2,
           });
-          const time = `${hours}:${minutes}`;
+        const minutes = (Math.floor(
+          ((position / oneHour) % 1).toString().split('.')[1]?.substr(0, 2) *
+          0.6)).toLocaleString('en-US', {
+          minimumIntegerDigits: 2,
+        });
+        info.innerHTML = `${hours}:${minutes}`;
+      }
+    });
 
-          info.innerHTML = time;
-        }
-      });
-
-      ruler.addEventListener('click', event => {
-        const position = event.clientX - rect.left;
-        console.log(`${Math.floor((position / rect.width) * 100)}%`);
-      });
-    };
-
-    trackRulerCursor('rulerId');
+    ruler.addEventListener('click', event => {
+      const position = event.clientX - rect.left;
+      console.log(`${Math.floor((position / rect.width) * 100)}%`);
+    });
   }
 });
 
